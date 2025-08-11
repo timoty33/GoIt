5152561357ecb5c418e3691432e81809ce858593
timoty33 <timotyduo@gmail.com>
2025-08-11 10:01:50 -0300

    alterações, criando documentação

--- goit/cmd/run.go
+++ goit/cmd/run.go
@@ -14,6 +14,7 @@ var runCmd = &cobra.Command{
 	Long:  "Inicia um super projeto criado com GoIt, e pode ter diferentes modos, como 'dev', 'test' ou 'linter'",
 
 	RunE: func(cmd *cobra.Command, args []string) error {
+		
 		// carregando as configs
 		configProject, _, err := utils.LoadJsonConfig()
 		if err != nil {
